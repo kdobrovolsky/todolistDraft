aab0cfe06c9b271b6bc9ed86452f1257c28da00c
kdobrovolsky <kdobrovolsky02@gmail.com>
2025-06-25 20:15:17 +0300

    update

---
+++ src/state/user-reducer.test.ts
@@ -0,0 +1,15 @@
+
+
+test ('user reducer should increment only age', () => {
+    const startState = {age: 20, childrenCount: 26, name: 'Kirill'}
+
+    const endState = userReducer(startState, {type: 'INCREMENT-AGE'})
+
+    expect(endState.age).toBe(21)
+    expect(endState.childrenCount).toBe(2)
+
+})
+
+test('user reducer should increment only childrenCount', () => {
+    const stateState = {age: 20, childrenCount: 2, name: 'Kirill'}
+})
